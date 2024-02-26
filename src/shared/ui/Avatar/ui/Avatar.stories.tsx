7a6752274179012a2191b3ae0a259ede99d3763d
Rebi1ion <flair0105@gmail.com>
2024-02-26 16:29:29 +0300

    fix screenshot tests

--- src/shared/ui/Avatar/ui/Avatar.stories.tsx
+++ src/shared/ui/Avatar/ui/Avatar.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { Avatar } from "./Avatar";
+import AvatarImg from "shared/assets/profile-img.jpg";
 
 const meta = {
   title: "shared/Avatar",
@@ -15,13 +16,13 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    src: "https://pic.rutubelist.ru/user/3b/27/3b2758ad5492a76b578f7ee072e4e894.jpg",
+    src: AvatarImg,
   },
 };
 
 export const Small: Story = {
   args: {
-    src: "https://pic.rutubelist.ru/user/3b/27/3b2758ad5492a76b578f7ee072e4e894.jpg",
+    src: AvatarImg,
     size: 50,
   },
 };
